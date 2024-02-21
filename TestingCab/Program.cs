e55e4404116a0a505032e56c857e0766564845b9
poojakashyap-2920 <pooja337026@gmail.com>
2024-02-21 22:04:11 +0530

    added

---
+++ TestingCab/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TestingCab
+{
+    internal class Program
+    {
+        static double costPerKM = 10;
+        static double costPerMin = 10;
+        static double MinFare = 5;
+        public double TotalFare(double distance, int time)
+        {
+
+            double totval = (distance * costPerKM) + (time * costPerMin);
+            if (totval < 5)
+            {
+                return MinFare;
+            }
+            else
+            {
+                return totval;
+
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
